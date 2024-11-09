14ba2bceaf31538424b51329e5ca8d083f6d92b7
Drovosek01 <drovosek01tnt@gmail.com>
2024-11-09 16:52:24 +0300

    Added function for detection archivers for using for unpacking

--- Windows/Unpacker.ps1
+++ Windows/Unpacker.ps1
@@ -11,7 +11,90 @@ param (
 # GLOBAL VARIABLES
 # =====
 
+$archiversDefaultPathes = @{
+    '7z' = 'C:\Program Files\7-Zip\7z.exe';
+    'bz' = 'C:\Program Files\Bandizip\bz.exe';
+    'rar' = 'C:\Program Files\WinRAR\Rar.exe';
+    'unrar' = 'C:\Program Files\WinRAR\UnRAR.exe'
+}
+
 
 # =====
 # FUNCTIONS
 # =====
+
+function detectDefaultArchivers {
+    foreach ($key in $archiversDefaultPathes.Keys) {
+        if (Test-Path $archiversDefaultPathes[$key]) {
+            Write-Host "Found default archiver: $($archiversDefaultPathes[$key]) - It will be used for unpacking"
+            return $archiversDefaultPathes[$key]
+        }
+    }
+}
+
+function isHashTableContainGivenFilename {
+    param (
+        [Parameter(Mandatory)]
+        [string]$filename
+    )
+    
+    foreach ($key in $archiversDefaultPathes.Keys) {
+        $archiverFilename = [System.IO.Path]::GetFileName($archiversDefaultPathes[$key])
+
+        if ($archiverFilename -eq $filename) {
+            return $true
+        }
+    }
+
+    return $false
+}
+
+function detectArchiver {
+    param (
+        [string]$archiverPath
+    )
+
+    $archiverWorkerPath = ''
+
+    $customArchiverFileName = [System.IO.Path]::GetFileName($archiverPath)
+
+
+    if ($archiverPath) {
+        $archiverFullPath = [System.IO.Path]::GetFullPath($archiverPath)
+        
+        if (Test-Path $archiverFullPath) {
+            Write-Host "Found given archiver: $archiverFullPath"
+            
+            if (isHashTableContainGivenFilename $customArchiverFileName) {
+                $archiverWorkerPath = $archiverFullPath
+                Write-Host "Script support using given archiver!"
+                break
+            } else {
+                Write-Host "Script not support using given archiver! Will search default archivers."
+                $archiverWorkerPath = detectDefaultArchivers
+            }
+        } else {
+            Write-Host "Not found given archiver: $archiverFullPath"
+            Write-Host "Will search default archivers."
+            $archiverWorkerPath = detectDefaultArchivers
+        }
+    } else {
+        $archiverWorkerPath = detectDefaultArchivers
+    }
+    
+    if (-not $archiverWorkerPath) {
+        Write-Error "Archivers not found. Install 7-zip or Bandipzip or WinRAR and relaunch script"
+        exit 1
+    } else {
+        return $archiverWorkerPath
+    }
+}
+
+
+try {
+    $archiverWorkerPath = detectArchiver $archiverPath
+    write-host "after $archiverWorkerPath"
+}
+catch {
+    <#Do this if a terminating exception happens#>
+}
